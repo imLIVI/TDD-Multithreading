538958c1d4de467c4db97fea2f631c61bb74da83
imLIVI <lesly120898@gmail.com>
2023-01-07 23:04:14 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,7 +4,10 @@ import java.util.Map;
 public class PhoneBook {
     private Map<String, String> phoneBook = new HashMap<>();
 
-    public int add(String name, String PhoneNumber) {
-        return 0;
+    public int add(String name, String phoneNumber) {
+        if (!phoneBook.containsKey(name))
+            phoneBook.put(name, phoneNumber);
+
+        return phoneBook.size();
     }
 }
